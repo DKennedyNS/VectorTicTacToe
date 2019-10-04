6008d9266788bbdb887874ecd5d23d245118bd6d
Donovan Kennedy <W0411531@campus.nscc.ca>
2019-10-03 22:55:56 -0300

    Apparently triple compare doesn't work...

--- main.cpp
+++ main.cpp
@@ -86,33 +86,34 @@ class Board{
         }//end parse input
 
     bool checkWin(){
+        //Check all possible win conditions
+        if(board[0][0]==board[1][1] && board[1][1]==board[2][2]){
+            winner = board[0][0];
+            return true;
+        }else if(board[0][2]==board[1][1] && board[1][1]==board[2][0]){
+            winner = board[0][2];
+            return true;
+        }else if(board[0][0]==board[0][1] && board[0][1]==board[0][2]){
+            winner = board[0][0];
+            return true;
+        }else if(board[1][0]==board[1][1] && board[1][1]==board[1][2]){
+            winner = board[1][0];
+            return true;
+        }else if(board[2][0]==board[2][1] && board[2][1]==board[2][2]){
+            winner = board[2][0];
+            return true;
+        }else if(board[0][0]==board[1][0] && board[1][0]==board[2][0]){
+            winner = board[0][0];
+            return true;
+        }else if(board[0][1]==board[1][1] && board[1][1]==board[2][1]){
+            winner = board[0][1];
+            return true;
+        }else if(board[0][2]==board[1][2] && board[1][2]==board[2][2]){
+            winner = board[0][2];
+            return true;
+        }else{
+            return false;}
 
-            if(board[0][0]==board[1][1]==board[2][2]){
-                winner = board[0][0];
-                return true;
-            }else if(board[0][2]==board[1][1]==board[2][0]){
-                winner = board[0][2];
-                return true;
-            }else if(board[0][0]==board[0][1]==board[0][2]){
-                winner = board[0][0];
-                return true;
-            }else if(board[1][0]==board[1][1]==board[1][2]){
-                winner = board[1][0];
-                return true;
-            }else if(board[2][0]==board[2][1]==board[2][2]){
-                winner = board[2][0];
-                return true;
-            }else if(board[0][0]==board[1][0]==board[2][0]){
-                winner = board[0][0];
-                return true;
-            }else if(board[0][1]==board[1][1]==board[2][1]){
-                winner = board[0][1];
-                return true;
-            }else if(board[0][2]==board[1][2]==board[2][2]){
-                winner = board[0][2];
-                return true;
-            }else{
-                return false;}
     }//end check win
 
     char getWinner(){
